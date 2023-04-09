fab74c782ef427bf19f2a257fd44face185af8fa
BrenoReisSantos <breno.battle@gmail.com>
2023-04-09 03:53:12 -0300

    feat: listagem de todos os monitores

--- src/HippocampusWeb/Data/HippocampuApiClient.cs
+++ src/HippocampusWeb/Data/HippocampuApiClient.cs
@@ -1,5 +1,6 @@
 using System.Net.Http.Json;
 using HippocampusWeb.Diplomat.HttpIn;
+using HippocampusWeb.Values;
 using HippocampusWeb.Values.Forms;
 
 namespace HippocampusWeb.Data;
@@ -7,6 +8,7 @@ namespace HippocampusWeb.Data;
 public interface IHippocampusApiClient
 {
     Task<RecipientMonitorCreatedDto?> PostRecipientMonitorAsync(RecipientMonitorCreationForm form);
+    Task<IEnumerable<MonitorsTableRow>?> GetMonitorsForTable();
 }
 
 public class HippocampuApiClient : HttpClient, IHippocampusApiClient
@@ -20,8 +22,11 @@ public class HippocampuApiClient : HttpClient, IHippocampusApiClient
     public async Task<RecipientMonitorCreatedDto?> PostRecipientMonitorAsync(RecipientMonitorCreationForm form)
     {
         var response = await this.PostAsJsonAsync("RecipientMonitors/", form);
+        if (!response.IsSuccessStatusCode)
+            return null;
         return await response.Content.ReadFromJsonAsync<RecipientMonitorCreatedDto>();
     }
 
-    // public async Task<>
+    public async Task<IEnumerable<MonitorsTableRow>?> GetMonitorsForTable() =>
+        await this.GetFromJsonAsync<IEnumerable<MonitorsTableRow>>("RecipientMonitors/list");
 }
--- src/HippocampusWeb/Values/MonitorsTableRow.cs
+++ src/HippocampusWeb/Values/MonitorsTableRow.cs
@@ -4,8 +4,12 @@ public class MonitorsTableRow
 {
     public Guid Id { get; init; }
     public string Name { get; init; } = string.Empty;
+    public string MacAddress { get; init; } = string.Empty;
     public RecipientType RecipientType { get; init; }
+    public int MaxHeight { get; init; }
+    public int MinHeight { get; init; }
     public int Level { get; init; }
-    public RecipientState RecipientState { get; init; } 
+    public RecipientState RecipientState { get; init; }
     public bool IsPumpOn { get; init; }
+    public string LinkedRecipientMonitorMacAddress { get; init; } = string.Empty;
 }
